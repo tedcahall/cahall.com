f1fc4d26293a6509c3ea682325b017ef64cda5d9
Ted Cahall <cahall@cahall.com>
2019-07-07 07:16:59 -0700

    first code

---
+++ html/racing/2011/MARRS-5/highslide/highslide.config.js
@@ -0,0 +1,40 @@
+/**
+*	Site-specific configuration settings for Highslide JS
+*/
+hs.graphicsDir = 'highslide/graphics/';
+hs.showCredits = false;
+hs.outlineType = 'custom';
+hs.allowMultipleInstances = false;
+hs.blockRightClick = true;
+hs.enableKeyListener = false;
+hs.captionEval = 'this.a.title';
+hs.registerOverlay({
+	html: '<div class="close-simple-white" onclick="return hs.close(this)" title="Close"></div>',
+	position: 'top right',
+	useOnHtml: true,
+	fade: 2 // fading the semi-transparent overlay looks bad in IE
+});
+
+
+
+// Add the slideshow controller
+hs.addSlideshow({
+	slideshowGroup: 'group1',
+	interval: 5000,
+	repeat: false,
+	useControls: true,
+	fixedControls: 'fit',
+	overlayOptions: {
+		opacity: 0.75,
+		position: 'bottom center',
+		offsetX: 0,
+		offsetY: -10,
+		hideOnMouseOut: true
+	}
+});
+
+// gallery config object
+var config1 = {
+	slideshowGroup: 'group1',
+	transitions: ['expand', 'crossfade']
+};
